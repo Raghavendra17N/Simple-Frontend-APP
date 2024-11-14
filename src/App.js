809f6d83a5faf9fd6b1701b558422aeee2e90f34
Raghavendra17N <raghava01995@gamil.com>
2024-11-14 15:00:09 +0530

    updated fileupload code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './FileUpload'; // Adjust the import path as needed
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Cloud Storage File Upload</h1>
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
